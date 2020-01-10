8b819129bda6fe6518948b8aafd38336e2b62f4d
shadmaan <shadmaan4f93@gmail.com>
2020-01-10 15:11:45 +0530

    MISC

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bootcamp.wsgi --log-file -
+web: gunicorn panelprovider.wsgi --log-file -
--- panelprovider/settings.py
+++ panelprovider/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import logging
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,4 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
 
 logger = logging.getLogger()
 logger.setLevel(LOG_LEVEL)
+django_heroku.settings(locals())
